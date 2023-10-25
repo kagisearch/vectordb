bb0225a75c5b981b9658189d94e87e8d4d2b37a3
Vladimir Prelovac <vprelovac@gmail.com>
2023-10-25 10:55:21 -0700

    pylint ignores

--- vectordb/__init__.py
+++ vectordb/__init__.py
@@ -1,1 +1,3 @@
+#pylint: disable = line-too-long, trailing-whitespace, trailing-newlines, line-too-long, missing-module-docstring, import-error, too-few-public-methods, too-many-instance-attributes, too-many-locals
+
 from .memory import Memory
