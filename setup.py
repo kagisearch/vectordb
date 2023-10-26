32218b9a33aac2c0399e972d142b6e43c61b25a0
Vladimir Prelovac <vprelovac@gmail.com>
2023-10-26 09:19:16 -0700

    0.1.7 - added support for distance

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vectordb2",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     install_requires=[
         "torch>=1.9.0",
