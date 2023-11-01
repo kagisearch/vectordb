6803367bc76f8d30bc94b7b31d4f7661e698df3b
Vladimir Prelovac <vprelovac@gmail.com>
2023-10-31 20:52:40 -0700

    0.1.9 - fix disk storage

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vectordb2",
-    version="0.1.8.1",
+    version="0.1.9",
     packages=find_packages(),
     install_requires=[
         "torch>=1.9.0",
