3eca2439d119e809a7359cfaaadd89e1900d0ee6
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-03-28 07:00:57 +0000

    docs: Add community health files

--- CODE_OF_CONDUCT.md
+++ CODE_OF_CONDUCT.md
@@ -60,8 +60,8 @@ representative at an online or offline event.
 
 Instances of abusive, harassing, or otherwise unacceptable behavior may be
 reported to the community leaders responsible for enforcement at
-[connor.stone@mila.quebec](mailto:connor.stone@mila.quebec). All complaints will
-be reviewed and investigated promptly and fairly.
+[connor.stone@mila.quebec](mailto:vlad@kagi.com). All complaints will be
+reviewed and investigated promptly and fairly.
 
 All community leaders are obligated to respect the privacy and security of the
 reporter of any incident.
---
+++ noxfile.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import shutil
+from pathlib import Path
+
+import nox
+
+DIR = Path(__file__).parent.resolve()
+
+nox.options.sessions = ["lint", "pylint", "tests", "build"]
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    """
+    Run the linter.
+    """
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
+
+
+@nox.session
+def pylint(session: nox.Session) -> None:
+    """
+    Run PyLint.
+    """
+    # This needs to be installed into the package environment, and is slower
+    # than a pre-commit check
+    session.install(".", "pylint")
+    session.run("pylint", "src", *session.posargs)
+
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    """
+    Run the unit and regular tests. Use --cov to activate coverage.
+    """
+    session.install(".[dev]")
+    session.run("pytest", *session.posargs)
+
+
+@nox.session
+def build(session: nox.Session) -> None:
+    """
+    Build an SDist and wheel.
+    """
+
+    build_p = DIR.joinpath("build")
+    if build_p.exists():
+        shutil.rmtree(build_p)
+
+    session.install("build")
+    session.run("python", "-m", "build")
