4e22f4a3eae3e5687db54dfd4c65cfbeb9a15914
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-03-28 04:07:12 +0000

    refactor: Move vectordb to src and update __init__

--- src/vectordb/__init__.py
+++ src/vectordb/__init__.py
@@ -1,1 +1,5 @@
 # pylint: disable = line-too-long, trailing-whitespace, trailing-newlines, line-too-long, missing-module-docstring, import-error, too-few-public-methods, too-many-instance-attributes, too-many-locals
+from ._version import version as VERSION  # noqa
+
+__version__ = VERSION
+__author__ = "kagisearch"
