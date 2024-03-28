58ec24c33bb895c209f7f1a40f5ebb3e8f64bbf8
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-03-28 08:25:18 +0000

    fix: Add modules to __init__

--- src/vectordb/_version.py
+++ src/vectordb/_version.py
@@ -3,7 +3,6 @@
 TYPE_CHECKING = False
 if TYPE_CHECKING:
     from typing import Tuple, Union
-
     VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
     VERSION_TUPLE = object
@@ -13,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev116"
-__version_tuple__ = version_tuple = (0, 1, "dev116")
+__version__ = version = '0.1.dev117'
+__version_tuple__ = version_tuple = (0, 1, 'dev117')
