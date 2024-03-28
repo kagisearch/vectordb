ce51bcabf21a9150a2f695da7ab094011b3a96d5
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-03-28 08:10:32 +0000

    chore: Pre-commit fixes

--- src/vectordb/_version.py
+++ src/vectordb/_version.py
@@ -13,5 +13,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev104"
-__version_tuple__ = version_tuple = (0, 1, "dev104")
+__version__ = version = "0.1.dev116"
+__version_tuple__ = version_tuple = (0, 1, "dev116")
