cafe7a6f624ca7bfb9249c5b4892fcf6230a0103
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-03-28 20:46:19 +0000

    refactor: Add Dockerfile and update devcontainer.json

--- .devcontainer/postBuild.sh
+++ .devcontainer/postBuild.sh
@@ -1,5 +1,11 @@
 # These commands will be run after the devcontainer is built.
 
+# Setup Conda environment
+conda env create -f ~/.devcontainer/SciPy2024/environment.yml # Create environment from environment.yml
+conda init bash # Initialize conda for bash
+source ~/.bashrc # Reload bash
+conda activate scipy2024 # Activate the environment
+
 # Install vectordb locally for development
 python3 -m pip install --user -r requirements.txt # Install required packages
 python3 -m pip install pre-commit nox # Install development tools
