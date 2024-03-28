ddfe3331356cf8595f9e682b0632eace0a06e746
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-03-28 03:11:26 +0000

    refactor: Remove extra whitespace

--- vectordb/__init__.py
+++ vectordb/__init__.py
@@ -1,2 +1,1 @@
 # pylint: disable = line-too-long, trailing-whitespace, trailing-newlines, line-too-long, missing-module-docstring, import-error, too-few-public-methods, too-many-instance-attributes, too-many-locals
-
