69666bf0b788f1209445a1665dab8e96092624a6
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-03-28 01:03:28 -0700

    refactor: Install env pkgs in postBuild

--- .devcontainer/postBuild.sh
+++ .devcontainer/postBuild.sh
@@ -1,5 +1,5 @@
 # These commands will be run after the devcontainer is built.
 
 # Install vectordb locally for development
-python3 -m pip install -e .
-apt-get install git ncdu wget curl
+python3 -m pip install --user -r requirements.txt # Install required packages
+python3 -m pip install -e . # Install vectordb locally
